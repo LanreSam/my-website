e0f2ff377673d723acbf697d124858ad96aabe06
LanreSam <ezechelanre@gmail.com>
2021-01-27 06:25:39 +0100

    working on the mail

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function sendMail(Request $request)
+    {
+        //validates user inputs in all the field provided in the contact form
+        $validatedata = $request->validate([
+            'name' => 'required|min:3|max:30',
+            'email' => 'required',
+            'phone' => 'required',
+            'message' => 'required'
+        ]);
+
+        //gets all the user information entered in the contact form
+        $details = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'message' => $request->message,
+        ];
+        //gets the user email only
+        $email = ['email' => $request->email];
+
+        //message to be sent to the user's email
+        $msg = "We have successfully recieved your mail and will reply you shortly";
+        //this sends the user message to the site's mail
+        Mail::to('ezechilanre@gmail.com')->send(new ContactMail($details));
+        //this sends a feedback reply to the user's email
+        //Mail::to($email)->send(new feedbackMail($msg));
+
+        return back()->with('mail-msg', "Mail sent successfully. We will reply you shortly.");
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,11 @@
     {{ $slot }}
 
 
+
     @livewireScripts
+    {{-- sweet alert library --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
+
     <!-- jQuery -->
     <script src="{{ asset('assets/js/jquery.js') }}"></script>
     <!--  plugins  -->
