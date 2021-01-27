03f4132c0cfda6c24691ab7aea934471f7bdf0ef
LanreSam <ezechelanre@gmail.com>
2021-01-27 10:25:34 +0100

    production

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,6 +29,16 @@
 
 <body>
 
+    {{-- <div id="preloader">
+    <div id="status">
+
+        <div class="preloader" aria-busy="true" aria-label="Loading, please wait." role="progressbar">
+        </div>
+
+        </div>
+    </div> --}}
+    <!-- ./Preloader -->
+
     {{ $slot }}
 
 
--- resources/views/livewire/index-component.blade.php
+++ resources/views/livewire/index-component.blade.php
@@ -1,15 +1,3 @@
-
-{{-- <div id="preloader">
-    <div id="status">
-
-        <div class="preloader" aria-busy="true" aria-label="Loading, please wait." role="progressbar">
-        </div>
-
-    </div>
-</div> --}}
-<!-- ./Preloader -->
-
-<!-- header -->
 <header class="navbar-fixed-top">
     <nav>
         <ul>
