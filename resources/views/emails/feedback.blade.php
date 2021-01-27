7ca6eef5c3f3b1c908179f0d2bdbc2daeae3a37d
LanreSam <ezechelanre@gmail.com>
2021-01-27 10:14:26 +0100

    production

--- resources/views/emails/feedback.blade.php
+++ resources/views/emails/feedback.blade.php
@@ -11,8 +11,8 @@
 </head>
 <body>
     <h2>Thanks for contacting me</h2>
-    <p>I will reach out to you shortly</p>
-    <a href="https://devshood.herokuapp.com" class="text-white">Visit Website</a>
+    <p>I will reach out to you as soon as possible</p>
+    <a href="https://ezechelanre.herokuapp.com" class="text-white">Visit Website</a>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
